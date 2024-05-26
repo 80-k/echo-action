ef7ddc31fa63b98681110fea3a0a768aeab12bda
80-k <jiant_leap@outlook.com>
2024-05-26 22:33:01 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import * as core from '@actions/core';
 
 const name = core.getInput('name');
 const result = `Hello, ${name}`;
+print("inp", name)
 print("This main.js is running...")
+print("output", to-the-world)
 core.setOutput('to-the-world', result);
